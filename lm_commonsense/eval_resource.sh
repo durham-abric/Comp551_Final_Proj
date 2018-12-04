bc91589211fc9182c21e600f26e6d30ef7356257
durham-abric <durham.abric@mail.mcgill.ca>
2018-12-03 20:42:22 -0500

    ...

--- lm_commonsense/eval_resource.sh
+++ lm_commonsense/eval_resource.sh
@@ -2,7 +2,7 @@
 
 read -p 'PID of Process: ' model_pid
 
-printResourceUse="top -p$model_pid -b -n 1 | awk '{printf "%s %s %s %s %s %s %s\n",$1,$5,$6,$7,$9,$(10),$(11)}' >> outputs/eval_resources.txt"
+printResourceUse="top -p$model_pid -b -n 1 >> outputs/eval_resources.txt"
 
 echo "Monitoring Started!"
 
